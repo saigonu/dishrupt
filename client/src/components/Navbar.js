4d956fd0652d84dde3883801bdf354a18b67dd8d
Sai <saipraneethgonuguntla@gmail.com>
2023-05-22 19:01:19 -0700

    update

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
+
+import logo from "../Logo.png";
+
+export const Navbar = () => {
+  const [cookies, setCookies] = useCookies(["access_token"]);
+  const navigate = useNavigate();
+
+  const logout = () => {
+    setCookies("access_token", "");
+    window.localStorage.clear();
+    navigate("/auth");
+  };
+
+  return (
+    <div className="navbar">
+    <div className="navbar-logo">
+        <img src={logo} alt="Logo" />
+      </div>
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/create-recipe">Create Recipe</Link>
+        <Link to="/saved-recipes">Saved Recipes</Link>
+      </div>
+      {!cookies.access_token ? (
+        <Link to="/auth" className="buton">Login / Register</Link>
+      ) : (
+        <div className="navbar-button-container">
+          <button className="logout-button" onClick={logout}>
+            Logout
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
---
+++ client/src/pages/saved-recipes.js
@@ -0,0 +1,5 @@
+export const SavedRecipes = () => {
+
+    return <div>SavedRecipe</div>
+
+};
